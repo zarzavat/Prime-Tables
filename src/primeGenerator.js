7be24cf31a4ff16f4ef99f3453d6614effde1e41
Mihai Anisiu <manisiu@gmail.com>
2017-02-05 18:24:15 +0000

    added naive prime generation method

--- src/primeGenerator.js
+++ src/primeGenerator.js
@@ -2,7 +2,27 @@ function PrimeGenerator() {
 }
 
 PrimeGenerator.prototype.generateFirstPrimes = function (n, callback) {
-    callback(null, Array(n).fill(2));
+    var currentNumberToCheck = 2;
+    var generatedPrimes = [];
+    while (generatedPrimes.length < n) {
+        if (isPrime(currentNumberToCheck)) {
+            generatedPrimes.push(currentNumberToCheck);
+        }
+        currentNumberToCheck += 1;
+    }
+    callback(null, generatedPrimes);
 };
+
+function isPrime(n, callback) {
+    if (n === 2) {
+        return true;
+    }
+    for (var i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
 module.exports = PrimeGenerator;
