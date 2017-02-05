aad19c3c63a92cfa90d678aa289a1f946a60641d
Mihai Anisiu <manisiu@gmail.com>
2017-02-05 16:51:43 +0000

    set up development environment

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
+var gutil = require('gulp-util');
+
+gulp.task('lint', function () {
+    gutil.log('linting');
+    return gulp.src(['**/*.js', '!node_modules/**', '!test/**'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('test', ['lint'], function () {
+    gutil.log('testing');
+    return gulp.src('test/*')
+        .pipe(mocha());
+});
+
+var node, busy = false;
+var fork = require('child_process').fork;
+gulp.task('respawn', ['lint', 'test'], function () {
+    if (busy) { return; }
+
+    function spawn() {
+        node = fork('.', { env: { NODE_ENV: 'development' } });
+        busy = false;
+    }
+
+    if (node && node.connected) {
+        busy = true;
+        node.once('exit', spawn);
+        node.kill();
+    } else {
+        spawn();
+    }
+});
+
+gulp.task('serve', function () {
+    gulp.watch('**/*.js', ['respawn']);
+}); 
---
+++ test/primeGenerator.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+describe('prime generation', function () {
+    it('should generate one prime', function () {
+        assert.fail();
+
+    })
+})
